67a08392e0c5ea4256c993da7f91afa6c6734f93
faresbat99 <faresbat99@gmail.com>
2023-04-07 01:08:52 +0200

    configuration

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+// define constance 
+
+// links 
+// BURL base url
+define("BURL","http://localhost/test/medical_services/");
+define("BURLA","http://localhost/test/medical_services/admin/");
+define("assets","http://localhost/test/medical_services/assets/");
+//base link
+define("BL",__DIR__."/");
+define("BLA",__DIR__.'/admin/');
+
+//connect to DB
+require_once(BL."functions/db.php");
